bf1b039acadf70598e84365a64a9911ecbb82476
snrylmz <senerov@senerov.com>
2023-06-04 21:30:42 +0300

    sitemap eklendi

---
+++ app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://senerov.com',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://senerov.com/post',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://senerov.com/post/ucretsiz-ssl-sertifikasi-kurulumu-lets-encrypt',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://senerov.com/post/digitalocean-ssh-key-olusturma',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://senerov.com/post/css-font-size-em-rem-vw-vh-px-percentage',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://senerov.com/post/digitalocean-droplet-olusturma',
+        lastModified: new Date(),
+      },
+    ];
+  }
